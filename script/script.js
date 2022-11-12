4a3a5b65cf0239d56dda43efa0195cff1b70bc5b
Nazarov Alexei <lesha6228nazarov@gmail.com>
2022-11-12 19:51:51 +0300

    js edits

--- script/script.js
+++ script/script.js
@@ -7,8 +7,10 @@ const buttonCloseUser = popupUser.querySelector('.popup__toggle');
 // попап element
 const popupElement = document.querySelector('.popup_type_element');
 const buttonCloseElement = popupElement.querySelector('.popup__toggle');
-const formElement = popupElement.querySelector('.form-element');
-// секция профиль
+const formAddCard = popupElement.querySelector('.form-element');
+const inputCardTitle = popupElement.querySelector('.popup__line_title'); 
+const inputCardLink = popupElement.querySelector('.popup__line_link');
+// секция профиль 
 const profile = document.querySelector('.profile')
 const nameProfile = profile.querySelector('.profile__name');
 const JobProfile = profile.querySelector('.profile__activity');
@@ -19,51 +21,31 @@ const popupImg = document.querySelector('.popup_type_img')
 const buttonCloseImg = popupImg.querySelector('.popup__toggle');
 const titleImg = popupImg.querySelector('.popup__description')
 const pictureImg = popupImg.querySelector('.popup__img')
+//template форма карточки 
+const elementsTemplate = document.querySelector('#elements-template').content;
+// место добавления новых карточек
+const elementsSection = document.querySelector('.elements__wrapper');
 //функция Октрытие модального
-const openPopup = popupElemen => {
-  popupElemen.classList.add('popup_opened')
+const openPopup = popup => {
+  popup.classList.add('popup_opened')
 }
 //функция закртие модального окна
-const closePopup = popupElemen => {
-  popupElemen.classList.remove('popup_opened');
+const closePopup = popup => {
+  popup.classList.remove('popup_opened');
 }
-// Октрытие модального окна User
-profileButtonEdit.addEventListener('click', () => {
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = JobProfile.textContent;
-  openPopup(popupUser);
-});
-// закртие модального окна User
-buttonCloseUser.addEventListener('click', () => {
-  closePopup(popupUser);
-});
-/* Изменения имени и дейтельности через модальным окно с сохранением и закрытием окна */
-function formSubmitUser(evt) {
+/*функция изменения имени и дейтельности через модальным окно с сохранением и закрытием окна */
+function submitEditProfileForm(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   JobProfile.textContent = jobInput.value;
   closePopup(popupUser);
 }
-formUser.addEventListener('submit', formSubmitUser);
-// Октрытие модального окна Element
-profileButtonCreate.addEventListener('click', () => {
-  openPopup(popupElement);
-});
-// закртие модального окна Element
-buttonCloseElement.addEventListener('click', () => {
-  closePopup(popupElement);
-});
-//закрытие модального окна img
-buttonCloseImg.addEventListener('click', () => {
-  closePopup(popupImg);
-});
 //функция формерования новой карточки с возможностью удаления и кнопкой лайк и открытием картинки
-function creatingNewCard(titleValue, linkValue) {
-  const elementsSection = document.querySelector('.elements__wrapper');
-  const elementsTemplate = document.querySelector('#elements-template').content; /* заменить на класс */
+function createCard(titleValue, linkValue) {
   const elementsElement = elementsTemplate.querySelector('.elements__element').cloneNode(true);
   elementsElement.querySelector('.elements__text').textContent = titleValue;
   elementsElement.querySelector('.elements__img').src = linkValue;
+  elementsElement.querySelector('.elements__img').alt = `фотография ${titleValue}`;
   /* лайк - не лайк */
   elementsElement.querySelector('.elements__button').addEventListener('click', (evt) => {
     evt.target.classList.toggle('elements__button_active');
@@ -76,49 +58,49 @@ function creatingNewCard(titleValue, linkValue) {
   elementsElement.querySelector('.elements__img').addEventListener('click', () => {
     titleImg.textContent = titleValue;
     pictureImg.src = linkValue;
+    pictureImg.alt = `фотография ${titleValue}`;
     openPopup(popupImg);
-  }); 
-  elementsSection.prepend(elementsElement);
+  });
+  return elementsElement
 }
-// добавления новой карточки на страницу через модальное окно 
-function formSubmitElement(evt) {
+// функция добавления карточки в DOM
+function addCard(card) {
+  elementsSection.prepend(card);
+}
+//функция добавления новой карточки на страницу через модальное окно 
+function submitAddCardForm(evt) {
   evt.preventDefault();
-  const title = document.querySelector('.popup__line_title');
-  const link = document.querySelector('.popup__line_link');
-  creatingNewCard(title.value, link.value);
-  link.value = '';
-  title.value = '';
+  addCard(createCard(inputCardTitle.value, inputCardLink.value));
   closePopup(popupElement);
 }
-formElement.addEventListener('submit', formSubmitElement);
-// массив с дефолтными карточками
-const initialCards = [
-  {
-    name: 'Ростов',
-    link: 'https://images.unsplash.com/photo-1661956600655-e772b2b97db4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
-  },
-  {
-    name: 'Казань',
-    link: 'https://images.unsplash.com/photo-1504615458222-979e04d69a27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80'
-  },
-  {
-    name: 'Смоленск',
-    link: 'https://images.unsplash.com/photo-1668101205453-97cd2ba2bb26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
-  },
-  {
-    name: 'Вологда',
-    link: 'https://images.unsplash.com/photo-1668123508592-d004d362dde3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80'
-  },
-  {
-    name: 'Санкт-петербург',
-    link: 'https://images.unsplash.com/photo-1555460285-763ba96917d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
-  },
-  {
-    name: 'Москва',
-    link: 'https://images.unsplash.com/photo-1513326738677-b964603b136d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bW9zY293fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
-  }
-];
-// формерование и добавление дефолтных карточек на страницу
+//формерование и добавление дефолтных карточек на страницу
 initialCards.forEach(element => {
-  creatingNewCard(element.name, element.link)
+  addCard(createCard(element.name, element.link));
 });
+// Октрытие модального окна User
+profileButtonEdit.addEventListener('click', () => {
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = JobProfile.textContent;
+  openPopup(popupUser);
+});
+// закртие модального окна User
+buttonCloseUser.addEventListener('click', () => {
+  closePopup(popupUser);
+});
+// Октрытие модального окна Element
+profileButtonCreate.addEventListener('click', () => {
+  formAddCard.reset();
+  openPopup(popupElement);
+});
+// закртие модального окна Element
+buttonCloseElement.addEventListener('click', () => {
+  closePopup(popupElement);
+});
+//закрытие модального окна img
+buttonCloseImg.addEventListener('click', () => {
+  closePopup(popupImg);
+});
+// слушатель формы user
+formUser.addEventListener('submit', submitEditProfileForm);
+// слушатель формы Element
+formAddCard.addEventListener('submit', submitAddCardForm);
